44c1763f600a44ceb74fbd6df633bb99dbb20e74
aschild <aaron.schild@gmail.com>
2012-01-29 23:11:00 -0500

    Initial test commit of garbage

---
+++ CodeEduGame/src/com/appspot/codeedugame/CodeEduGameServlet.java
@@ -0,0 +1,13 @@
+package com.appspot.codeedugame;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class CodeEduGameServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
+}
