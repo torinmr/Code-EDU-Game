d462502d4f6a591c247e4f55395a087c4224acc9
Torin Rudeen <trudeen@princeton.edu>
2012-01-30 13:24:16 -0500

    Modified API and continued implementation

--- CodeEduGame/src/com/appspot/codeedugame/Blackjack.java
+++ CodeEduGame/src/com/appspot/codeedugame/Blackjack.java
@@ -1,5 +1,9 @@
 package com.appspot.codeedugame;
 
+import java.util.List;
+
+import com.appspot.codeedugame.deck.PokerDeck;
+import com.appspot.codeedugame.deck.PokerCard;
 import com.google.appengine.api.datastore.Key;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
@@ -14,24 +18,38 @@ public class Blackjack {
     private Key key;
 
 	@Persistent
-	Deck deck;
+	PokerDeck deck;
 	
 	@Persistent
-	Deck playerCards;
+	PokerDeck discardPile;
 	
 	@Persistent
-	Deck dealerCards;
+	PokerDeck playerCards;
+	
+	@Persistent
+	PokerDeck dealerCards;
 	
 	@Persistent
 	int playerMoney;
 	
 	@Persistent
 	int bid;
-
+	
+	@Persistent
+	boolean roundOver;
+	
 	public Blackjack(int playerMoney) {
 		this.playerMoney = playerMoney;
 		this.bid = 0;
-		// insert code for initializing deck.
+		
+		this.deck = PokerDeck();
+		this.deck.constructStandardDeck();
+		this.deck.shuffle();
+		
+		this.playerCards = PokerDeck();
+		this.dealerCards = PokerDeck();
+		this.discardPile = PokerDeck();
+		this.roundOver = false;
 	}
 	
 	// Accessors.
@@ -40,15 +58,15 @@ public class Blackjack {
 	}
 	
 	public int deckSize() {
-		// insert code.
+		return deck.getSize();
 	}
 	
-	public Deck getPlayerCards() {
-		// insert
+	public List<PokerCard> getPlayerCards() {
+		return playerCards.getOrdering();
 	}
 	
-	public Deck getDealerCards() {
-		// insert
+	public List<PokerCard> getDealerCards() {
+		return dealerCards.getOrdering();
 	}
 	
 	public int getPlayerMoney() {
@@ -58,28 +76,94 @@ public class Blackjack {
 	public int getBid() {
 		return bid;
 	}
+	
+	public boolean isOver() {
+		return roundOver;
+	}
 
 	// real methods
 	
 	// updates the game state to reflect a move of "hit"
-	// returns true if successful, returns false in case of error.
+	// returns true if successful, returns false in case of illegal play.
 	public boolean hit() {
+		if (roundOver) {
+			return false;
+		}
+		if (playerCards.getSize == 0) {
+			makeBid(0);
+		}
 		
+		dealPlayerCard();
+		if (isBust()) {
+			playerLose();
+		}
 	}
 	
 	// updates the game state to reflect a move of "stand"
-	// returns true if successful, false in case of error.
+	// returns false in case of illegal play, true otherwise.
+	public boolean stand() {
+		if (roundOver) {
+			return false;
+		}
+		if (playerCards.getSize == 0) {
+			makeBid(0);
+		}
+		
+		dealerFinish();
+		return true;
+	}
+	
+	// updates the game state to reflect a move of "double down"
+	// returns false in case of illegal play, true otherwise.
 	public boolean doubleDown() {
+		if (roundOver) {
+			return false;
+		}
+		
+		if (playerCards.getSize() > 2) {
+			return false;
+		}
+		
+		if (playerCards.getSize() == 0) {
+			makeBid(0);
+		}
+		
 		if (bid > playerMoney) {
 			return false;
 		}
 		playerMoney -= bid;
 		bid *= 2;
 		
-		return hit();
+		hit();
+		stand();
+		return true;
 	}
 	
-	public boolean makeBid() {
+	// sets the bid amount to the given value. Returns false in case of
+	// illegal play, true otherwise.
+	public boolean makeBid(int bidAmount) {
+		if (playerCards.getSize() > 0) {
+			return false;
+		}
+		if (bidAmount > playerMoney) {
+			return false;
+		}
+		
+		bid = bidAmount;
+		playerMoney -= bidAmount;
+
+		dealPlayerCard();
+		dealDealerCard();
+		dealPlayerCard();
+		dealDealerCard();
+		return true;
+	}
+}
+
+// private methods
+private void dealPlayerCard() {
+	if (deck.draw(card) == null) {
 		
 	}
+	return;
 }
