86409960002e0db930b7bacbb01e8c142152c82a
aschild <aaron.schild@gmail.com>
2012-02-01 17:17:55 -0500

    AppEngine emailer added

---
+++ CodeEduGame/src/com/appspot/codeedugame/EmailerServlet.java
@@ -0,0 +1,47 @@
+package com.appspot.codeedugame;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class EmailerServlet extends HttpServlet {
+    
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String msgBody = req.getParameter("comment");
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("noreply@codeedugame.appspotmail.com", "Code EDU Game"));
+            msg.addRecipient(Message.RecipientType.TO,
+                             new InternetAddress("javajackdev@gmail.com", "Miles Dunham"));
+            msg.setSubject("New comment from " + name + " at " + email);
+            msg.setText(msgBody);
+            Transport.send(msg);
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
