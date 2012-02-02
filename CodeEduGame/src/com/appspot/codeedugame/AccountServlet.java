4471ca1483bc4c8f2db6c9c0c29b6dedb82e40bf
aschild <aaron.schild@gmail.com>
2012-02-01 19:49:00 -0500

    AccountServlet added to repo

---
+++ CodeEduGame/src/com/appspot/codeedugame/AccountServlet.java
@@ -0,0 +1,15 @@
+package com.appspot.codeedugame;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class AccountServlet extends HttpServlet {
+    
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        
+    }
+
+}
